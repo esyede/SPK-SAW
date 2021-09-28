7e5d78a51e55538f244b6ac3b8e882c817b93337
rhmtty <rohmat.triy@gmail.com>
2021-09-28 13:27:01 +0700

    first commit

---
+++ database/migrations/2021_09_27_064658_create_criterias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCriteriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('criterias', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('kode')->nullable()->unique();
+            $table->enum('attribute', ['benefit', 'cost']);
+            $table->integer('bobot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('criterias');
+    }
+}
