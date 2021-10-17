753c98d72c03b2f8497aca07bcbfadd5a7d75591
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2021-10-17 12:09:24 +0700

    reset

--- resources/views/layouts/backend/partials/footer.blade.php
+++ resources/views/layouts/backend/partials/footer.blade.php
@@ -2,8 +2,7 @@
     <div class="app-footer">
         <div class="app-footer__inner">
             <div class="app-footer-left">
-                <strong>Copyright &copy; {{ date('Y') }} <a href="#">P-Team</a>.</strong> All
-                rights reserved.
+                Copyright &copy; {{ date('Y') }} <a href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a>.
             </div>
         </div>
     </div>
