03e8b66a7a542379334710901675d152c114ae6b
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2021-10-18 23:02:51 +0700

    design login

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,34 @@
 @extends('layouts.frontend.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Reset password</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Kirim link reset password
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="register-photo">
+    <div class="form-container">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <h2 class="mb-3"><strong>Reset Password</strong></h2>
+            <div class="form-group"> 
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-success btn-block">
+                    Kirim link reset password
+                </button>
+            </div>
+        </form>
+        <div class="image-holder"></div>
     </div>
-</div>
+</div>	
 @endsection
--- resources/views/backend/rating/index.blade.php
+++ resources/views/backend/rating/index.blade.php
@@ -22,26 +22,175 @@
                         <span class="btn-icon-wrapper pr-2 opacity-7">
                             <i class="fas fa-plus-circle fa-w-20"></i>
                         </span>
-                        {{ __('Tambah Penilaian') }}
+                        {{ __('Tambah Data') }}
                     </a>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- ========== Nilai ========== -->
     <div class="row">
         <div class="col-md-12">
+            <div class="d-flex mb-2">
+                <div class="h6">
+                    Nilai
+                </div>
+                <div class="ml-auto">
+                    <button type="button" class="btn btn-primary btn-sm">
+                        Salin
+                    </button>
+                    <button type="button" class="btn btn-success btn-sm">
+                        Excel
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                        Pdf
+                    </button>
+                    <button type="button" class="btn btn-secondary btn-sm">
+                        Print
+                    </button>
+                </div>
+            </div>
             <div class="main-card mb-3 card">
                 <div class="table-responsive">
-                    <table id="datatable" class="align-middle mb-0 table table-borderless table-striped table-hover">
+                    <table class="datatable align-middle mb-0 table table-borderless table-striped table-hover">
                         <thead>
-                        <tr>
-                            <th class="text-center">#</th>
-                            <th>Nama Kriteria</th>
-                            <th class="text-center">Kode Kriteria</th>
-                            <th class="text-center">Status</th>
-                            <th class="text-center">Joined At</th>
-                            <th class="text-center">Actions</th>
-                        </tr>
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>Karyawan</th>
+                                <th class="text-center">Kriteria</th>
+                                <th class="text-center">Nilai</th>
+                                <th class="text-center">Selisih</th>
+                                <th class="text-center">Nilai GAP</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+    
+        <!-- ========== Rata - Rata ========== -->
+        <div class="col-md-6">
+            <div class="d-flex mb-2">
+                <div class="h6">
+                    Nilai Rata - Rata Per Faktor
+                </div>
+                <div class="ml-auto">
+                    <button type="button" class="btn btn-primary btn-sm">
+                        Salin
+                    </button>
+                    <button type="button" class="btn btn-success btn-sm">
+                        Excel
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                        Pdf
+                    </button>
+                    <button type="button" class="btn btn-secondary btn-sm">
+                        Print
+                    </button>
+                </div>
+            </div>
+            <div class="main-card mb-3 card">
+                <div class="table-responsive">
+                    <table class="datatable align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>Karyawan</th>
+                                <th class="text-center">Kriteria</th>
+                                <th class="text-center">Faktor</th>
+                                <th class="text-center">Rata - Rata</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- ========== Rata - Rata ========== -->
+        <div class="col-md-6">
+            <div class="d-flex mb-2">
+                <div class="h6">
+                    Nilai Total & Akhir
+                </div>
+                <div class="ml-auto">
+                    <button type="button" class="btn btn-primary btn-sm">
+                        Salin
+                    </button>
+                    <button type="button" class="btn btn-success btn-sm">
+                        Excel
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                        Pdf
+                    </button>
+                    <button type="button" class="btn btn-secondary btn-sm">
+                        Print
+                    </button>
+                </div>
+            </div>
+            <div class="main-card mb-3 card">
+                <div class="table-responsive">
+                    <table class="datatable align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>Karyawan</th>
+                                <th class="text-center">Kriteria</th>
+                                <th class="text-center">Nilai Total</th>
+                                <th class="text-center">Nilai Akhir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ========== Ranking ========== -->
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <div class="d-flex mb-2">
+                <div class="h6">
+                    Ranking Karyawan
+                </div>
+                <div class="ml-auto">
+                    <button type="button" class="btn btn-primary btn-sm">
+                        Salin
+                    </button>
+                    <button type="button" class="btn btn-success btn-sm">
+                        Excel
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                        Pdf
+                    </button>
+                    <button type="button" class="btn btn-secondary btn-sm">
+                        Print
+                    </button>
+                </div>
+            </div>
+            <div class="main-card mb-3 card">
+                <div class="table-responsive">
+                    <table class="datatable align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>Nama Karyawan</th>
+                                <th class="text-center">Ranking</th>
+                                <th class="text-center">Action</th>
+                            </tr>
                         </thead>
                         <tbody>
                             
@@ -52,3 +201,14 @@
         </div>
     </div>
 @endsection
+
+@push('js')
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // Datatable
+            $(".datatable").DataTable();
+        });
+    </script>
+@endpush
