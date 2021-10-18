64bc171dab3c23c0dd736fe17dc70f49e30ebd67
rhmtty <rohmat.triy@gmail.com>
2021-10-18 23:48:23 +0700

    fix: change redirect url to dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * The controller namespace for the application.
