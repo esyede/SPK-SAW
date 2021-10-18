509b2f4f39670bf4152d4e8abdbcbe15a0aaafd6
Bayu29 <bsegara2903@gmail.com>
2021-10-18 23:20:24 +0700

    change ratingcontroller and rating group routing to evaluationcontroller and evaluation route

--- routes/backend.php
+++ routes/backend.php
@@ -56,13 +56,13 @@ Route::group(['as' => 'sub-criteria.', 'prefix' => 'sub-criteria'], function() {
 });
 
 // Gap Score
-Route::group(['as' => 'gap-score.', 'prefix' => 'gap-score'], function() {
+Route::group(['as' => 'value-weighting.', 'prefix' => 'value-weighting'], function() {
     Route::get('/', [GapScoreController::class, 'index'])->name('index');
 });
 
 // Penilaian
-Route::group(['as' => 'rating.', 'prefix' => 'rating'], function() {
-    Route::get('/', [RatingController::class, 'index'])->name('index');
+Route::group(['as' => 'evaluation.', 'prefix' => 'evaluation'], function() {
+    Route::get('/', [EvaluationController::class, 'index'])->name('index');
 });
 
 // Security
