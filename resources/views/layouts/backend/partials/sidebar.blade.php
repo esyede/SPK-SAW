c04832e50d94ca99b3188c6a7959ceb480aef4dd
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2021-10-20 20:48:42 +0700

    remake sidebar

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -34,7 +34,74 @@
     <div class="scrollbar-sidebar">
         <div class="app-sidebar__inner">
             <ul class="vertical-nav-menu">
-                <x-backend-side-menu />
+                <li>
+                    <a href="/dashboard" class="{{ Request::is('dashboard*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Dashboard
+                    </a>
+                </li>
+                
+                @if( Auth::user()->role_id == 2)
+                <li>
+                    <a href="/evaluation" class="{{ Request::is('evaluation*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-note"></i>
+                        Penilaian
+                    </a>
+                </li>
+                @endif
+
+                @if( Auth::user()->role_id == 1 )
+                <li>
+                    <a href="/roles" class="{{ Request::is('roles*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-check"></i>
+                        Roles
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">SPK</li>
+                <li>
+                    <a href="/criteria" class="{{ Request::is('criteria*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-news-paper"></i>
+                        Kriteria
+                    </a>
+                </li>
+                <li>
+                    <a href="/sub-criteria" class="{{ Request::is('sub-criteria*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-note2"></i>
+                        Sub Kriteria
+                    </a>
+                </li>
+                <li>
+                    <a href="/integrity" class="{{ Request::is('integrity*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-graph3"></i>
+                        Pembobotan Nilai
+                    </a>
+                </li>
+                <li>
+                    <a href="/users" class="{{ Request::is('users*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-id"></i>
+                        Pegawai
+                    </a>
+                </li>
+                <li>
+                    <a href="/evaluation" class="{{ Request::is('evaluation*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-note"></i>
+                        Penilaian
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Pengaturan</li>
+                <li>
+                    <a href="/settings/general" class="{{ Request::is('setting/general*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-settings"></i>
+                        Umum
+                    </a>
+                </li>
+                <li>
+                    <a href="/backups" class="{{ Request::is('backups*') ? 'mm-active' : '' }}">
+                        <i class="metismenu-icon pe-7s-cloud"></i>
+                        Backup
+                    </a>
+                </li>
+                @endif
             </ul>
         </div>
     </div>
