606337e2f3576e6b85f149dd3514f3f546b5bd29
Bayu29 <bsegara2903@gmail.com>
2021-11-05 23:33:04 +0700

    perbaikan pada seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CriteriaSeeder::class);
         $this->call(SubCriteriaSeeder::class);
-        $this->call(PerfomanceAssesmentSeeder::class);
         $this->call(IntegritiesSeeder::class);
     }
 }
