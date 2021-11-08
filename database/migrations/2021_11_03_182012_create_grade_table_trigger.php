092e608315d0c970a830712a8d7cd1d2bdaf8916
rhmtty <rohmat.triy@gmail.com>
2021-11-08 10:54:24 +0700

    fix: trigger grade

--- database/migrations/2021_11_03_182012_create_grade_table_trigger.php
+++ database/migrations/2021_11_03_182012_create_grade_table_trigger.php
@@ -14,8 +14,8 @@ class CreateGradeTableTrigger extends Migration
      */
     public function up()
     {
-        DB::unprepared("CREATE TRIGGER create_grade AFTER INSERT on users FOR EACH ROW EXECUTE PROCEDURE BEGIN
-        THEN INSERT INTO grades (user_id, total_grade_value, created_at, updated_at)  VALUES(NEW.id, 0.0, now(), now()); END");
+        DB::unprepared("CREATE TRIGGER create_grade AFTER INSERT ON users FOR EACH ROW EXECUTE PROCEDURE BEGIN
+        INSERT INTO grades (user_id, total_grade_value, created_at, updated_at)  VALUES(NEW.id, 0.0, now(), now()); END");
     }
 
     /**
